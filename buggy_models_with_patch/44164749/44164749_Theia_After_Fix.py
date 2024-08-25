c0e99a8f8d90b2589455c3bdba484fe02bcf6893
anoau <141194732+anoau@users.noreply.github.com>
2024-08-25 18:24:45 -0500

    Add files via upload

---
+++ buggy_models_with_patch/44164749/44164749_Theia_After_Fix.py
@@ -0,0 +1,35 @@
+
+import time
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation,BatchNormalization
+from keras.optimizers import SGD
+from keras.initializers import RandomNormal
+from sklearn.datasets import make_multilabel_classification
+from sklearn.preprocessing import MinMaxScaler
+
+
+X, y = make_multilabel_classification(n_samples = 10000, n_classes = 11,n_labels=3, random_state=42)
+sc = MinMaxScaler()
+X = sc.fit_transform(X)
+
+model = Sequential()
+model.add(Dense(5000, input_dim=X.shape[1]))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.1))
+model.add(Dense(600))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.1))
+model.add(Dense(y.shape[1]))
+model.add(Activation('softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+start_time = time.time()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy',])
+
+model.fit(X,y,epochs=10,batch_size=64)
